8be6a8873b6d34550e8ff938ffe49427611a3202
LAPTOP-HL9Q62TA\vuman <vumanhdung579@gmail.com>
2021-01-23 16:10:28 +0700

    jnjn
    (cherry picked from commit 72d1743c38d9fa1a1940a66f3a9d19b62b57e920)

--- ManagerProject/Controllers/HomeController.cs
+++ ManagerProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ManagerProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application descrjhbjhbhjbhjiption page.";
 
             return View();
         }
